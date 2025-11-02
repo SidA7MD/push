2bda96becea66aac56c644e2d76a40898b3f32a6
SidA7MD <24110@supnum.mr>
2025-11-02 19:58:02 +0000

    docks: ajouter readme.md

--- index.js
+++ index.js
@@ -1,40 +1,48 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
-import simpleGit from "simple-git";
 import random from "random";
+import simpleGit from "simple-git";
 
 const path = "./data.json";
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+const DAYS = 20;          // 20 days × 5 commits/day = 100 commits
+const COMMITS_PER_DAY = 5;
+const TOTAL_COMMITS = DAYS * COMMITS_PER_DAY;
 
-  const data = {
-    date: date,
-  };
+const START = moment("2025-02-01");
+const END = moment("2025-03-31");
 
+let commitCount = 0;
+
+const makeCommit = (date) => {
+  const data = { date };
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
+    simpleGit().add([path]).commit(date, { "--date": date }, () => {
+      commitCount++;
+      if (commitCount === TOTAL_COMMITS) {
+        console.log("✅ All commits done. Pushing...");
+        return simpleGit().push();
+      }
+    });
   });
 };
 
-const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+const createCommits = () => {
+  for (let day = 0; day < DAYS; day++) {
+    const commitDay = START.clone().add(day, "days");
 
-  const data = {
-    date: date,
-  };
-  console.log(date);
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
-  });
+    // Skip days outside Feb-Mar 2025 range if needed
+    if (commitDay.isBefore(START) || commitDay.isAfter(END)) continue;
+
+    for (let i = 0; i < COMMITS_PER_DAY; i++) {
+      const randomHour = random.int(8, 20);
+      const randomMinute = random.int(0, 59);
+      const date = commitDay.clone().hour(randomHour).minute(randomMinute).format();
+
+      console.log("Commit:", date);
+      makeCommit(date);
+    }
+  }
 };
 
-makeCommits(100);
+createCommits();
